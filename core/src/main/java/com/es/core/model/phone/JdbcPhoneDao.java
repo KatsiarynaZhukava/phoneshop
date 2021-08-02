488d4d54534baf38e85a7073d4acb03dafa4dad9
KatsiarynaZhukava <46075648+KatsiarynaZhukava@users.noreply.github.com>
2021-08-02 22:55:31 +0300

    Implement Task 2.1: DAO

--- core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
+++ core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
@@ -1,27 +1,55 @@
 package com.es.core.model.phone;
 
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
-public class JdbcPhoneDao implements PhoneDao{
+public class JdbcPhoneDao implements PhoneDao {
+    private static final String SELECT_PHONES_WITH_COLORS_QUERY = "select * from phones " +
+                                                                  "left join phone2color on phones.id = phone2color.phoneId " +
+                                                                  "left join colors on phone2color.colorId = colors.id";
+    private static final String DELETE_PHONE2COLOR_QUERY = "delete * from phone2color where phoneId = ?";
+    private static final String INSERT_INTO_PHONE2COLOR_QUERY = "insert into phone2color (phoneId, colorId) values (?, ?)";
+    private static final String INSERT_INTO_PHONES_QUERY = "insert into phones (brand, model, price, displaySizeInches, weightGr, lengthMm, widthMm, heightMm, announced, deviceType, os, displayResolution, pixelDensity, displayTechnology, backCameraMegapixels, frontCameraMegapixels, ramGb, internalStorageGb, batteryCapacityMah, talkTimeHours, standByTimeHours, bluetooth, positioning, imageUrl, description, id) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    private static final String UPDATE_PHONES_QUERY = "update phones set brand = ?, model = ?, price = ?, displaySizeInches = ?, weightGr = ?, lengthMm = ?, widthMm = ?, heightMm = ?, announced = ?, deviceType = ?, os = ?, displayResolution = ?, pixelDensity = ?, displayTechnology = ?, backCameraMegapixels = ?, frontCameraMegapixels = ?, ramGb = ?, internalStorageGb = ?, batteryCapacityMah = ?, talkTimeHours = ?, standByTimeHours = ?, bluetooth = ?, positioning = ?, imageUrl = ?, description = ? where id = ?";
+
     @Resource
     private JdbcTemplate jdbcTemplate;
 
-    public Optional<Phone> get(final Long key) {
-        throw new UnsupportedOperationException("TODO");
+    public Optional<Phone> get( final Long key ) {
+        List<Phone> phones = jdbcTemplate.query(SELECT_PHONES_WITH_COLORS_QUERY + " WHERE phones.id = ?", new Object[] { key }, new PhoneExtractor());
+        return phones.stream().findFirst();
     }
 
-    public void save(final Phone phone) {
-        throw new UnsupportedOperationException("TODO");
+    public void save( final Phone phone ) {
+        Object[] fields = new Object[] { phone.getBrand(), phone.getModel(), phone.getPrice(), phone.getDisplaySizeInches(),
+                                         phone.getWeightGr(), phone.getLengthMm(), phone.getWidthMm(), phone.getHeightMm(),
+                                         phone.getAnnounced(), phone.getDeviceType(), phone.getOs(), phone.getDisplayResolution(),
+                                         phone.getPixelDensity(), phone.getDisplayTechnology(), phone.getBackCameraMegapixels(),
+                                         phone.getFrontCameraMegapixels(), phone.getRamGb(), phone.getInternalStorageGb(),
+                                         phone.getBatteryCapacityMah(), phone.getTalkTimeHours(), phone.getStandByTimeHours(),
+                                         phone.getBluetooth(), phone.getPositioning(), phone.getImageUrl(), phone.getDescription(), phone.getId() };
+        Optional<Phone> foundPhone = get(phone.getId());
+        if (foundPhone.isPresent()) {
+            jdbcTemplate.update(UPDATE_PHONES_QUERY, fields);
+            jdbcTemplate.update(DELETE_PHONE2COLOR_QUERY, phone.getId());
+        } else {
+            jdbcTemplate.update(INSERT_INTO_PHONES_QUERY, fields);
+        }
+        for (Color color: phone.getColors()) {
+            jdbcTemplate.update(INSERT_INTO_PHONE2COLOR_QUERY, phone.getId(), color.getId());
+        }
     }
 
-    public List<Phone> findAll(int offset, int limit) {
-        return jdbcTemplate.query("select * from phones offset " + offset + " limit " + limit, new BeanPropertyRowMapper(Phone.class));
+    public List<Phone> findAll( int offset, int limit ) {
+        List<Phone> phones = jdbcTemplate.query(SELECT_PHONES_WITH_COLORS_QUERY + " OFFSET ? LIMIT ?", new Object[] { offset, limit }, new PhoneExtractor());
+        return phones.stream()
+                     .filter(phone -> phone.getId() != null)
+                     .collect(Collectors.toList());
     }
 }
---
+++ core/src/main/java/com/es/core/model/phone/PhoneExtractor.java
@@ -0,0 +1,36 @@
+package com.es.core.model.phone;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.ResultSetExtractor;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhoneExtractor implements ResultSetExtractor<List<Phone>> {
+
+    @Override
+    public List<Phone> extractData( final ResultSet resultSet ) throws SQLException, DataAccessException {
+        List<Phone> phones = new ArrayList();
+        while (resultSet.next()) {
+            long id = resultSet.getLong("id");
+            Phone phone = phones.stream()
+                                .filter(phone1 -> id == phone1.getId())
+                                .findFirst()
+                                .orElse(null);
+            if (phone == null) {
+                phone = new BeanPropertyRowMapper<>(Phone.class).mapRow(resultSet, resultSet.getRow());
+                phone.setId(resultSet.getLong("id"));
+                phones.add(phone);
+            }
+
+            if (resultSet.getLong("colorId") != 0 || resultSet.getString("code") != null) {
+                Color color = new ColorRowMapper().mapRow(resultSet, resultSet.getRow());
+                phone.getColors().add(color);
+            }
+        }
+        return phones;
+    }
+}
