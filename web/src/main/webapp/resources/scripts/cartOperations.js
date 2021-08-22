4f19a2943c877a135ea1c5dba96cce905e50a669
KatsiarynaZhukava <kzhukova171120@gmail.com>
2021-08-22 20:32:36 +0300

    Final update fix

--- web/src/main/webapp/resources/scripts/cartOperations.js
+++ web/src/main/webapp/resources/scripts/cartOperations.js
@@ -3,7 +3,7 @@ function isInt(value) {
     return !isNaN(value) && (function(x) { return (x | 0) === x; })(parseFloat(value))
 }
 
-function cartOperations(form) {
+function addPhone(form) {
     let requestedQuantity = form.elements["requestedQuantity"].value;
     let phoneId = form.elements["phoneId"].value;
 
@@ -39,35 +39,23 @@ function cartOperations(form) {
     }
 }
 
-function removeCartItem(phoneId) {
-    let requestedQuantity = document.getElementById(`quantity${phoneId}`).value;
-    if(!isInt(requestedQuantity)) {
-        requestedQuantity = 0;
-    }
-    $.ajax({
-        url: contextPath + "/ajaxCart",
-        type: "DELETE",
-        data: JSON.stringify({requestedQuantity, phoneId}),
-        dataType: "json",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        contentType: "application/json",
-        success: function (data) {
-            $("#cartBtn").text("My cart: " + data.totalQuantity + " items $ " + data.totalCost.toFixed(1));
-
-            let tableRow = document.getElementById(`cartRow${phoneId}`);
-            tableRow.remove();
-            if (document.getElementsByTagName("tr").length === 1) {
-                document.getElementsByTagName("table")[0].remove();
-                document.getElementById("cartButtonsContainer").remove();
-
-                p = document.createElement("p");
-                p.className = "no-items-message";
-                p.innerText = "No items in your cart";
-                document.getElementById("cartItemsContainer").appendChild(p);
+window.addEventListener("load", function() {
+    let updateForm = document.getElementById("updateCartForm");
+    updateForm.addEventListener("submit", function(e) {
+        let allInputs = document.getElementsByTagName("input");
+        let inputs = [];
+        for (let i = 0; i < allInputs.length; i++) {
+            if(allInputs[i].id.indexOf(".requestedQuantity") === allInputs[i].id.length - ".requestedQuantity".length) {
+                inputs.push( allInputs[i]);
+                document.getElementById("errorMessage" + allInputs[i].id.slice("items".length, allInputs[i].id.length - ".requestedQuantity".length ) ).innerHTML = "";
             }
         }
-    });
-}
+        for(let i = 0; i < inputs.length; i++) {
+            if (!isInt(inputs[i].value)) {
+                e.preventDefault();
+                document.getElementById("updateMessage").innerHTML = "Error updating the cart";
+                document.getElementById("errorMessage" + inputs[i].id.slice("items".length, inputs[i].id.length - ".requestedQuantity".length ) ).innerHTML = "Quantity should be an integer number";
+            }
+        }
+    })
+});
